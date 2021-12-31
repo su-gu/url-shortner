d3d71599021f229ee1ff3e87870a606c773dc878
subh <subhasishgupta970@gmail.com>
2022-01-01 00:57:28 +0530

    This is a commit message

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask
 from database_init import db
 from routes import short
 
+app = Flask(__name__)
 
 
-def create_app(config_file='settings.py'):
-    app = Flask(__name__)
-    app.config.from_pyfile(config_file)
-    db.init_app(app)
-    app.register_blueprint(short)
-    return app 
+    
+app.config.from_pyfile("settings.py")
+db.init_app(app)
+app.register_blueprint(short)
 
 
 if __name__ == "__main__":
-    create_app()
+    app.run(debug=True)
