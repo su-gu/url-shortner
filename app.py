31ac95aeb6070d6f915f959070bdce72cac5aef7
subh <subhasishgupta970@gmail.com>
2021-12-31 21:20:46 +0530

    add app.run

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def create_app(config_file='settings.py'):
     app.config.from_pyfile(config_file)
     db.init_app(app)
     app.register_blueprint(short)
-    # app.run(port=54268)
+    app.run()
     return app 
 
 
